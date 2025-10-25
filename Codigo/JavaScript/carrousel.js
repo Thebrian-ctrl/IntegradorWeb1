8170f7e0c5eae4e9dd889082d36bbc1374152da4
Brayan Arce <arcebrayan@gmail.com>
2025-10-25 01:05:54 -0300

    Creacion de archivos css y js externos

---
+++ Codigo/JavaScript/carrousel.js
@@ -0,0 +1,39 @@
+let imagenes = [
+            "../../Imagenes/carrusel/img1.jpg",
+            "../../imagenes/carrusel/img2.webp",
+            "../../imagenes/carrusel/img3.jpg",
+            "../../imagenes/carrusel/img4.webp"
+        ];
+        let cont = 0;
+
+        function carrusel(carrucelContainer) {
+            carrucelContainer.addEventListener('click', e => {
+                let atras = carrucelContainer.querySelector('.atras'),
+                    adelante = carrucelContainer.querySelector('.adelante'),
+                    img = carrucelContainer.querySelector('img'),
+                    tgt = e.target;
+
+                if (tgt == atras) {
+                    if (cont > 0) {
+                        img.src = imagenes[cont - 1];
+                        cont--;
+                    } else {
+                        img.src = imagenes[imagenes.length - 1];
+                        cont = imagenes.length - 1;
+                    }
+                } else if (tgt == adelante) {
+                    if (cont < imagenes.length - 1) {
+                        img.src = imagenes[cont + 1];
+                        cont++;
+                    } else {
+                        img.src = imagenes[0];
+                        cont = 0;
+                    }
+                }
+            });
+        }
+
+        document.addEventListener("DOMContentLoaded", () => {
+            let carrucelContainer = document.querySelector('.carrucelContainer');
+            carrusel(carrucelContainer);
+        });
